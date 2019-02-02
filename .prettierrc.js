27425651fb28f1c328c728a426bbf1b6b69a4e6a
Aurimas L <aurimas@wow.lt>
2019-02-02 17:54:27 +0200

    eslint, prettier, browserlist, ie9 polyfill

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    printWidth: 80,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: false,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: 'avoid',
+};
